5fa22222dcc6daeeff26a6e66d05373b701ea27e
TolMalware <tolmachdanil@gmail.com>
2019-03-19 22:26:44 +0300

    write tests

--- homework08/test.py
+++ homework08/test.py
@@ -6,10 +6,10 @@ import queue
 
 
 class MyTestCase(unittest.TestCase):
-    def generate_data(self):
+    def generate_data(self, len_q, len_list):
         q = queue.Queue()
-        for _ in range(30):
-            array = [random.randint(0, 100) for _ in range(100000)]
+        for _ in range(len_q):
+            array = [random.randint(0, 100) for _ in range(len_list)]
             q.put(array)
         return q
 
@@ -17,8 +17,27 @@ class MyTestCase(unittest.TestCase):
         data.sort()
 
     def test_count_worker(self):
-        worker = Pool(0, 0, 500)
-        self.assertEqual(9, worker.map(self.task,self.generate_data())[0])
+        worker = Pool()
+        self.assertEqual(9, worker.map(self.task, self.generate_data(30, 100000))[0])
+
+    def test_count_worker_max(self):
+        worker = Pool(1, 2, 512)
+        self.assertEqual(2, worker.map(self.task, self.generate_data(30, 100000))[0])
+
+    @unittest.expectedFailure
+    def test_count_worker_min(self):
+        worker = Pool(15, 20, 512)
+        self.assertEqual(2, worker.map(self.task, self.generate_data(30, 100000))[0])
+
+    def test_memory(self):
+        worker = Pool()
+        self.assertLessEqual(45, int(worker.map(self.task, self.generate_data(30, 100000))[1]))
+
+    def test_max_memory(self):
+        worker = Pool()
+        count_worker = worker.map(self.task, self.generate_data(30, 100000))[0]
+        mem_for_worker = worker.map(self.task, self.generate_data(30, 100000))[1]
+        self.assertLessEqual(count_worker * mem_for_worker, 500)
 
 
 if __name__ == '__main__':
