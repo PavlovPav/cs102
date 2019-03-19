55b5364569b4a2f9a0ab86c401974248a14415f7
TolMalware <tolmachdanil@gmail.com>
2019-03-19 22:26:21 +0300

    fix bugs

--- homework08/main.py
+++ homework08/main.py
@@ -3,7 +3,7 @@ import psutil
 
 
 class Pool():
-    def __init__(self, min_workers, max_workers, mem_usage):
+    def __init__(self, min_workers=1, max_workers=10, mem_usage=500):
         self.min_workers = min_workers
         self.max_workers = max_workers
         self.mem_usage = mem_usage
@@ -15,20 +15,37 @@ class Pool():
         proc.start()
 
         proc_info = psutil.Process(proc.pid)
-        self.mem = proc_info.memory_info().rss / 1024 / 1024  #return in Mb
+        self.memory = proc_info.memory_info().rss / 1024 / 1024  # return in Mb
         # print(psutil.cpu_percent(interval=None))
 
         procs[0].join()
 
-        self.defer_count = int(self.mem_usage / self.mem - self.mem_usage / self.mem / 100 * 10)
-        self.defer_count = 1 if self.defer_count==0 else self.defer_count
+        self.worker_count = int(self.mem_usage / (self.memory + self.memory / 100 * 10))
+        self.worker_count = 1 if self.worker_count == 0 else self.worker_count
+
+        if self.worker_count > self.max_workers: self.worker_count = self.max_workers
+        if self.worker_count < self.min_workers:
+            raise Exception("The number of required workers is less than the minimum specified.")
+
+        for _ in range(self.worker_count):
+            if args.empty():
+                for proc in procs:
+                    proc.join()
+                return self.worker_count, self.memory
+            proc = Process(target=function, args=(args.get(),))
+            procs.append(proc)
+            proc.start()
+
 
         while not args.empty():
-            for _ in range(self.defer_count):
+            for idx, proc in enumerate(procs):
                 if args.empty(): break
-                proc = Process(target=function, args=(args.get(),))
-                procs.append(proc)
-                proc.start()
-            for proc in procs:
-                proc.join()
-        return self.defer_count,self.mem
+                if not proc.is_alive():
+                    new_proc = Process(target=function, args=(args.get(),))
+                    procs[idx] = new_proc
+                    new_proc.start()
+
+        for proc in procs:
+            proc.join()
+
+        return self.worker_count, self.memory
